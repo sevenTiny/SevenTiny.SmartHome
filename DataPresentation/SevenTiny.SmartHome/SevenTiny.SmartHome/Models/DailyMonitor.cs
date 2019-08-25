35d38adcd133f447ed8c90447661f204fd6317b3
seventiny <seventiny@foxmail.com>
2019-08-25 20:26:56 +0800

    add ef core

---
+++ DataPresentation/SevenTiny.SmartHome/SevenTiny.SmartHome/Models/DailyMonitor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SevenTiny.SmartHome.Models
+{
+    public partial class DailyMonitor
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public int? Year { get; set; }
+        public int? Month { get; set; }
+        public int? Day { get; set; }
+        public int? Hour { get; set; }
+        public double? Temperature { get; set; }
+        public double? Humidity { get; set; }
+    }
+}
